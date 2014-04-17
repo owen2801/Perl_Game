fc4c65bc8f378f8848b53f9ed9fff5d31a9c911d
owen2801 <owen12888@yahoo.com.hk>
2014-04-18 00:10:27 +0800

    x

--- sdl_codes/game_loop.pl
+++ sdl_codes/game_loop.pl
@@ -12,13 +12,23 @@ my @height_upper=(); #A dynamic array to store the height of upper rectangle
 my @height_lower=(); #A dynamic arrary to store the height of lower retangle 
 my $rect_dist=120; #The distance between two rectangles
 my $rect_width=60; #The width of a rectangle
-my @coord_x=(20, 200, 380); #A dynamic arrat to store the x-coordinate of drawing rectangles
+my @coord_x=(320, 500); #A dynamic arrat to store the x-coordinate of drawing rectangles
+
+my $started = 0; #state if first mouse click is clicked
+my $velocity =0; # velocity of the bird, move top if negative
+my $acceleration =0; #acceleration of user movement
+my $user_pos =200;	#the y cord of the user
+my $USER_WIDTH = 40;# const of user pic width
+my $USER_HEIGHT = 40;#const of user pic height
+my $lower_upper = 140;
+
+my $score = 0; # saving score
 
 #initialize the program, assign corresponding values.
 for ($i=0;$i<@coord_x;$i++)
 {
-	push @height_upper, int(rand(200)) + 40;
-	push @height_lower, 280-$height_upper[$i];
+	push @height_upper, int(rand(320 - $lower_upper)) + 40;
+	push @height_lower, (400-$lower_upper)-$height_upper[$i];
 	push @upper_rect, [$coord_x[$i], 0, $rect_width, $height_upper[$i]];
 	push @lower_rect, [$coord_x[$i], 400-$height_lower[$i], $rect_width, $height_lower[$i]];
 }
@@ -32,6 +42,12 @@ sub get_events
      while (SDL::Events::poll_event($event))   
      {   
         $quit=1 if $event->type==SDL_KEYDOWN;   
+		if ($event->type==SDL_MOUSEBUTTONDOWN){
+			$started =1;
+			$acceleration =-2;
+			$velocity = 0;
+		}
+	    $quit=1 if $event->type==SDL_QUIT;
      }   
 }
 #move the rectangles in the while loop
@@ -48,8 +64,8 @@ sub rect_move
 	if($coord_x[@coord_x - 1] < (400-$rect_dist-$rect_width))
 	{
 		push @coord_x, 400;
-		push @height_upper, int(rand(200)) + 40;
-		push @height_lower, 280-$height_upper[$i];
+		push @height_upper, int(rand(320-$lower_upper)) + 40;
+		push @height_lower, (400-$lower_upper)-$height_upper[@height_upper - 1];
 	}
 	#every time 1 pixel movement for every rectangle
 	for ($i = 0; $i < @coord_x; $i++)
@@ -69,12 +85,47 @@ sub render
 		$app->draw_rect($upper_rect[$i],$color);
     	$app->draw_rect($lower_rect[$i],$color);
 	}
+	my $pic=SDL::Image::load('user.jpg');
+	$pic= SDLx::Surface->new(surface =>$pic);
+    $pic->blit($app,[0,0,$USER_WIDTH,$USER_HEIGHT],[175,$user_pos,$USER_WIDTH,$USER_HEIGHT]);
+	
 	$app->update();
 }
+
+#calculate the user new position
+sub cal_user_pos
+{
+		$velocity = $velocity + $acceleration;
+		$acceleration =0.5 if($velocity < -4);
+		$user_pos = $user_pos + $velocity 	if($started == 1);
+		if(($user_pos <0 ) || ($user_pos > 360 )){
+			gameover();
+		}
+}
+
+#check if user touch the blocks
+sub check_coll(){
+	for($i=0; $i<@upper_rect; $i++){
+		if(($upper_rect[$i][0] > 175 - $rect_width) && ($upper_rect[$i][0] < 215) ){
+			if(($height_upper[$i] > $user_pos) || ( $user_pos + $USER_HEIGHT > $height_upper[$i]  + $lower_upper)){
+			gameover();
+			}
+		}
+	}
+}
+
+#handle when game over
+sub gameover(){
+	$quit =1; 
+	print 'Game Over! scorce = ' ,$score;
+}
+
 #while loop, runs every 0.03 sec
 while (!$quit){   
       get_events();
-	  rect_move();
+	  rect_move() if($started == 1);
+	  cal_user_pos();
 	  render();
-      select(undef, undef, undef, 0.03);	  
+	  check_coll();
+      select(undef, undef, undef, 0.02);	  
     } 
