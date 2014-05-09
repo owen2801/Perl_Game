597ad17f8cf26c02bd3eddf8b48a2bdbd16ee17e
owen2801 <owen12888@yahoo.com.hk>
2014-05-09 11:47:30 +0800

    update

--- sdl_codes/game_loop.pl
+++ sdl_codes/game_loop.pl
@@ -2,39 +2,58 @@
 use SDL;
 use SDLx::App;
 use SDL::Event;
-
-my $app=SDLx::App->new(w=>400,h=>500,d=>32,t=>"Flappy Me");
-my $color =[108,108,108,108];
-my $drawing=0;
-my @upper_rect=(); #A dynamic array to store the properties of Upper Rectangle [x-coordinate, y-coordinate, width, height]
-my @lower_rect=(); #A dynamic array to store the properties of lower Rectangle [x-coordinate, y-coordinate, width, height]
-my @height_upper=(); #A dynamic array to store the height of upper rectangle
-my @height_lower=(); #A dynamic arrary to store the height of lower retangle 
-my $rect_dist=120; #The distance between two rectangles
-my $rect_width=60; #The width of a rectangle
-my @coord_x=(320, 500); #A dynamic arrat to store the x-coordinate of drawing rectangles
-
-my $started = 0; #state if first mouse click is clicked
-my $velocity =0; # velocity of the bird, move top if negative
-my $acceleration =0; #acceleration of user movement
-my $user_pos =200;	#the y cord of the user
-my $USER_WIDTH = 40;# const of user pic width
-my $USER_HEIGHT = 40;#const of user pic height
-my $lower_upper = 140;
-
-my $score = 0; # saving score
-
-#initialize the program, assign corresponding values.
-for ($i=0;$i<@coord_x;$i++)
-{
-	push @height_upper, int(rand(320 - $lower_upper)) + 40;
-	push @height_lower, (400-$lower_upper)-$height_upper[$i];
-	push @upper_rect, [$coord_x[$i], 0, $rect_width, $height_upper[$i]];
-	push @lower_rect, [$coord_x[$i], 400-$height_lower[$i], $rect_width, $height_lower[$i]];
-}
+use Term::ANSIColor;
+use Win32::Sound;
 
 my $event=SDL::Event->new();   
-my $quit=0;   
+my $quit=2;
+# prompt for user icon:
+
+print 'please type the path to your user icon(Recommend: JPG Format, 40x40):';
+my $user_pic = <STDIN>;
+chomp($user_pic);
+$user_pic ='user.jpg' if($user_pic eq '');
+
+
+sub initialize
+{
+	$app=SDLx::App->new(w=>400,h=>500,d=>32,t=>"Flappy Me");
+	$color =[108,108,108,108];
+	$rect_dist=120; #The distance between two rectangles
+	$rect_width=60; #The width of a rectangle
+
+	$started = 0; #state if first mouse click is clicked
+	$USER_WIDTH = 40;# const of user pic width
+	$USER_HEIGHT = 40;#const of user pic height
+	$lower_upper = 140;
+	$life = 1;
+	$score = 0; # saving score
+	$current_score = 0; # saving score in int
+	$final_score = 0;
+	#initialize the program, assign corresponding values.
+
+	while($quit == 2)
+	{
+		$quit = 0;
+		@upper_rect=(); #A dynamic array to store the properties of Upper Rectangle [x-coordinate, y-coordinate, width, height]
+		@lower_rect=(); #A dynamic array to store the properties of lower Rectangle [x-coordinate, y-coordinate, width, height]
+		@height_upper=(); #A dynamic array to store the height of upper rectangle
+		@height_lower=(); #A dynamic arrary to store the height of lower retangle
+		@coord_x=(780, 960); #A dynamic arrat to store the x-coordinate of drawing 
+		$velocity =0; # velocity of the bird, move top if negative
+		$acceleration =0; #acceleration of user movement
+		$user_pos =200;	#the y cord of the user
+		#initialize the program, assign corresponding values.
+		for ($i=0;$i<@coord_x;$i++)
+		{
+			push @height_upper, int(rand(320 - $lower_upper)) + 40;
+			push @height_lower, (400-$lower_upper)-$height_upper[$i];
+			push @upper_rect, [$coord_x[$i], 0, $rect_width, $height_upper[$i]];
+			push @lower_rect, [$coord_x[$i], 400-$height_lower[$i], $rect_width, $height_lower[$i]];
+		}
+		game_loop();
+	}
+}
 
 sub get_events
 {
@@ -46,10 +65,12 @@ sub get_events
 			$started =1;
 			$acceleration =-0.7;
 			$velocity = 0;
+			fly();
 		}
 	    $quit=1 if $event->type==SDL_QUIT;
      }   
 }
+
 #draw background
 sub draw_bg
 {
@@ -63,6 +84,7 @@ sub draw_bg
 	$sky->draw_circle_filled([150,50],25,[255,255,0,255]);
 	$sky->blit($app);
 }
+
 #move the rectangles in the while loop
 sub rect_move
 {
@@ -88,19 +110,22 @@ sub rect_move
 		$lower_rect[$i] = [$coord_x[$i], 400-$height_lower[$i], $rect_width, $height_lower[$i]];
 	}
 }
+
 #draw the rectangles
 sub render
 {
 	#draw a full screen black rectangle to cover all objects
 	#$app->draw_rect([0,0,400,400], [0,0,0,0]);
 	draw_bg();
+	
 	#draw the new rectangles
 	for ($i = 0; $i< @coord_x; $i++)
 	{
 		$app->draw_rect($upper_rect[$i],$color);
     	$app->draw_rect($lower_rect[$i],$color);
 	}
-	my $pic=SDL::Image::load('user.jpg');
+
+	my $pic=SDL::Image::load($user_pic);
 	$pic= SDLx::Surface->new(surface =>$pic);
     $pic->blit($app,[0,0,$USER_WIDTH,$USER_HEIGHT],[175,$user_pos,$USER_WIDTH,$USER_HEIGHT]);
 	
@@ -114,32 +139,93 @@ sub cal_user_pos
 		$acceleration =0.5 if($velocity < -6);
 		$user_pos = $user_pos + $velocity 	if($started == 1);
 		$user_pos = 0 if ($user_pos < 0);
-		gameover() if($user_pos > 360);
+		if($user_pos > 360){
+			if ($life > 1 ){
+					$life = $life - 1;
+					print "Opps, your current number of life is $life. Start over";
+					$quit = 2;
+					$started = 0;
+					dead();
+				}else{
+					gameover();
+				}
+		}
 }
 
 #check if user touch the blocks
 sub check_coll(){
-	for($i=0; $i<@upper_rect; $i++){
+	FOO:{
+	for($i=0; $i<@upper_rect; $i++)
+	{
 		if(($upper_rect[$i][0] > 175 - $rect_width) && ($upper_rect[$i][0] < 215) ){
 			if(($height_upper[$i] > $user_pos) || ( $user_pos + $USER_HEIGHT > $height_upper[$i]  + $lower_upper)){
-			gameover();
+				if ($life > 1 ){
+					$life = $life - 1;
+					print "Opps, your remaining number of life is $life. Wait 3 seconds to play again\n";
+					$quit = 2;
+					$started = 0;
+					dead();
+					last FOO;
+				}else{
+					gameover();
+				}
+			}else{
+				$score = $score + 1;
 			}
 		}
 	}
+	}
 }
+
+#cal life
+sub lifeleft(){
+		$current_score = int($score/60);
+	  	if ( $current_score != $final_score){
+	  		$final_score = $current_score;
+	  		if ( $final_score % 2 == 0){
+	  			$life = $life + 1;
+	  		}
+	  		print "current score = $final_score & life = $life. \n";
+	  	}
+	  
+}
+
 
 #handle when game over
 sub gameover(){
-	$quit =1; 
-	print 'Game Over! scorce = ' ,$score;
+	dead();
+	$quit = 1;
+	print 'Game Over! scorce = ' , int($score/60);
 }
 
-#while loop, runs every 0.03 sec
-while (!$quit){   
+#play fly sound
+sub fly()
+{
+	Win32::Sound::Volume('100%');
+	Win32::Sound::Play("fly-test.wav", SND_ASYNC);
+}
+
+#play dead sound
+sub dead()
+{
+	Win32::Sound::Volume('100%');
+	Win32::Sound::Play("dead-test.wav");
+	Win32::Sound::Stop();
+}
+
+
+initialize();
+#while loop
+sub game_loop
+{
+	while (!$quit)
+	{   
       get_events();
 	  rect_move() if($started == 1);
 	  cal_user_pos();
 	  render();
+	  lifeleft();
 	  check_coll();
-      select(undef, undef, undef, 0.01);	  
-    } 
+      select(undef, undef, undef, 0.007);	  
+    }
+}
